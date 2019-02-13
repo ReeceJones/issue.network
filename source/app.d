255791d802fe0803e528c61751720644a4dcc52a
ReeceJones <reece_jones@icloud.com>
2019-02-12 18:11:38 -0800

    initial commit

---
+++ source/app.d
@@ -0,0 +1,19 @@
+import vibe.d;
+
+
+
+shared static this()
+{
+	auto router = new URLRouter;
+	router.get("/", staticTemplate!"home.dt");
+	router.get("/iron-triangle", staticTemplate!"iron-triangle.dt");
+	router.get("/linkage-institutions", staticTemplate!"linkage-institutions.dt");
+
+	router.get("*", serveStaticFiles("public/"));
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 9069;
+	settings.bindAddresses = ["::1", "0.0.0.0"];
+
+	listenHTTP(settings, router);
+}
